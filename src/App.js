304c2a218d3c4c7b03f95af6aa1ad3f3852d8c9f
Honchar007 <54860596+Honchar007@users.noreply.github.com>
2021-04-07 19:28:05 +0300

    style nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+//  Import
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
+//  Styles
+import './styles/App.css'
 
 function App() {
   return (
